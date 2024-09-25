029d15030f76963f9e8be20dc77e53add4e2e082
Elizabeth Travers <melocus@hotmail.co.uk>
2024-09-25 13:24:41 +0000

    FIX - Fixed typo in allowed hosts

--- Scales/settings.py
+++ Scales/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj0)3hcrodd3a)jtxy$4p$=q_dv1^(1svzhmhf=jk_-vm+99q6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [8000-elizabeth97-scalestails-w9kj6jw2yz5.ws.codeinstitute-ide.net', 'scales-and-tails-b02a32c5979d.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = 
-['https://*.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['8000-elizabeth97-scalestails-w9kj6jw2yz5.ws.codeinstitute-ide.net', 'scales-and-tails-b02a32c5979d.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = [
+'https://*.codeinstitute-ide.net',
 'https://*.herokuapp.com']
 
 
@@ -77,11 +82,15 @@ WSGI_APPLICATION = 'Scales.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':{
+        dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
