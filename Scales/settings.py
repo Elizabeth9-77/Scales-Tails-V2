5571d53e16e51913dedd8a665fe1eb1b7dedc4bf
Elizabeth Travers <melocus@hotmail.co.uk>
2024-09-26 15:59:49 +0000

    FIX - Fixed local server style issue

--- Scales/settings.py
+++ Scales/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-elizabeth97-scalestails-w9kj6jw2yz5.ws.codeinstitute-ide.net', 'scales-and-tails-b02a32c5979d.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = [
