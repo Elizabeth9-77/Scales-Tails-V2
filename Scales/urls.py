e76d1fae7f6d13e4e29f9fc04a64eb443e945544
Elizabeth Travers <melocus@hotmail.co.uk>
2024-10-11 16:26:25 +0000

    CHANGE - changed default home page

--- Scales/urls.py
+++ Scales/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("about/", include("about.urls"), name="about-urls"),
+    path("forum/", include("forum.urls"), name="forum-urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('forum.urls'), name='forum-urls'),
+    path('', include('about.urls'), name='about-urls'),
 ]
