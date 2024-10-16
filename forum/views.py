dcbfe750c1e9ad304b9ea5057e12f44c180e3f7d
Elizabeth Travers <melocus@hotmail.co.uk>
2024-10-16 01:42:05 +0000

    ADD- added boa image and care sheet to forum page

--- forum/views.py
+++ forum/views.py
@@ -11,6 +11,12 @@ class PostList(generic.ListView):
     template_name = "forum/index.html"
     paginate_by = 6
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['welcome_text'] = "Welcome to my Boa care forum!"
+        context['welcome_image'] = "images/pexels-ivan-rojas-1655669943-28290883.webp"
+        return context
+
 
 def post_detail(request, slug):
     """
